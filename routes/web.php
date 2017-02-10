144aa025b280df2353d742e464b2c37afe9045a7
Aurélien Boncenne <aurelien.boncenne@gmail.com>
2017-02-10 14:37:22 +0100

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/hello', function () {
-    return view('welcome');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::resource('/article', 'ArticleController');
+
+Route::get('/user', function() {
+    return view('user');
+});
+
+Route::get('/admin', function() {
+    return view('admin');
 });
